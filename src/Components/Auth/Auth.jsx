e2374f492a8f6a2b7fd1c8c231134e167c39c043
McKay Waters <mckaypaswaters@gmail.com>
2019-12-05 23:27:36 -0700

    added login ternary and state

--- src/Components/Auth/Auth.jsx
+++ src/Components/Auth/Auth.jsx
@@ -4,8 +4,24 @@ import google from '../../assets/google-logo.png'
 
 export default class Auth extends Component {
     state = {
-
+        email: '',
+        name: '',
+        password: '',
+        continueClicked: false
     }
+
+    handleChange(e, key){
+        this.setState({
+            [key]: e.target.value
+        })
+    }
+
+    continueButton(){
+        this.setState({
+            continueClicked: true
+        })
+    }
+
     render(){
         return(
             <div className="auth">
@@ -16,9 +32,15 @@ export default class Auth extends Component {
                     </div>
                     <div className="login-box">
                         <h3>Sign up to Boardin'</h3>
-                        <input type="text" placeholder='Enter email'/>
+                        <input onChange={e => this.handleChange(e, 'email')} type="text" placeholder='Enter email'/>
+                        {this.state.continueClicked && this.state.email !== '' ? 
+                        <>
+                        <input onChange={e => this.handleChange(e, 'name')} type="text" placeholder='Enter full name'/>
+                        <input onChange={e => this.handleChange(e, 'password')} type="password" placeholder='Create password'/>
+                        </> : <></>
+                        }
                         <h5>By signing up, you confirm that you've read and accepted our <span>Terms of Service</span> and <span>Privacy Policy</span></h5>
-                        <button className='continue-button'>Continue</button>
+                        <button onClick={() => this.continueButton()} className='continue-button'>Continue</button>
                         <h4>OR</h4>
                         <button className="google-row"><img src={google} alt=""/><span>Continue with Google</span></button>
                         <h4 className='already'>Already have an account? Log in</h4>
